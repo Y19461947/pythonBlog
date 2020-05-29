7cd7379469a85eb9bde92a53d671946764b19da5
harry2018 <levili1946@gmail.com>
2020-05-29 13:20:12 +0800

    '数据库配置'

--- heimastudyblog/heimastudyblog/settings.py
+++ heimastudyblog/heimastudyblog/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'heimastudyblog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'harry',
+        'PASSWORD': '123456',
+        'NAME': 'blog_test'
+     }
 }
 
 
